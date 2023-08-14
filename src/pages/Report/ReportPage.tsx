5fa3da7a15c74708240b365abb45d9251c22d401
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-08-14 14:01:39 +0700

    update

--- src/pages/Report/ReportPage.tsx
+++ src/pages/Report/ReportPage.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import DescriptionIcon from "@mui/icons-material/Description";
 import { useNavigate } from "react-router-dom";
 
 function ReportPage() {
@@ -8,8 +8,12 @@ function ReportPage() {
     <div className="w-full flex flex-col">
       <div className="font-bold m-3">Report List</div>
       <ul className="w-full h-automx-3 float-left">
-        <li onClick={()=>navigate("/report/notes")} className="list-none ml-3 border rounded-md bg-white float-left w-1/3 shadow-md px-3 py-5 cursor-pointer hover:px-5 duration-100 hover:py-6">
-          <h4 className="text-gray-800 font-semibold">Notes</h4>
+        <li
+          onClick={() => navigate("/report/notes")}
+          className="list-none ml-3 items-center flex border rounded-md bg-white float-left w-1/3 shadow-md px-3 py-5 cursor-pointer hover:px-5 duration-100 hover:py-6"
+        >
+          <DescriptionIcon style={{fontSize:16}} />
+          <h4 className="text-gray-800 font-semibold ml-1">Notes</h4>
         </li>
       </ul>
     </div>
