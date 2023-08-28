8e88469f043c4fadbf2fa06b3f8a9c2b826df11b
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-08-28 15:33:48 +0700

    update

--- src/pages/Permission/FormPermissionPage.tsx
+++ src/pages/Permission/FormPermissionPage.tsx
@@ -7,16 +7,15 @@ import {
   IconButton,
   InputComponent,
   TimeLineVertical,
+  ToggleBodyComponent,
 } from "../../components/atoms";
-import { IValue } from "../../components/atoms/InputComponent";
+import { IListInput, IValue } from "../../components/atoms/InputComponent";
 import { LoadingComponent } from "../../components/moleculs";
 import moment from "moment";
 import { AlertModal, LocalStorage, Meta } from "../../utils";
 
 import { IListIconButton } from "../../components/atoms/IconButton";
 import Swal from "sweetalert2";
-
-
 
 const FormPermissionPage: React.FC = () => {
   let { id } = useParams();
@@ -33,9 +32,64 @@ const FormPermissionPage: React.FC = () => {
   const [history, setHistory] = useState<any[]>([]);
   const [isChangeData, setChangeData] = useState<boolean>(false);
 
+  const allowList: IListInput[] = [
+    { name: "Branch", value: "branch" },
+    { name: "User", value: "user" },
+    { name: "Customer", value: "customer" },
+    { name: "Customer Group", value: "customergroup" },
+    { name: "User Group", value: "usergroup" },
+  ];
+
+  const docList: IListInput[] = [
+    { name: "Branch", value: "branch" },
+    { name: "Visit", value: "visit" },
+    { name: "Callsheet", value: "callsheet" },
+    { name: "User", value: "user" },
+    { name: "Customer", value: "customer" },
+    { name: "Customer Group", value: "customergroup" },
+    { name: "User Group", value: "usergroup" },
+    { name: "Role Profile", value: "roleprofile" },
+    { name: "Schedule", value: "schedule" },
+    { name: "Memo", value: "memo" },
+    { name: "Contact", value: "contact" },
+    { name: "Notes", value: "notes" },
+  ];
+
+  // User
+  const [userList, setUserList] = useState<IListInput[]>([]);
+  const [userPage, setUserPage] = useState<Number>(1);
+  const [userLoading, setUserLoading] = useState<boolean>(true);
+  const [userMoreLoading, setUserMoreLoading] = useState<boolean>(false);
+  const [userHasMore, setUserHasMore] = useState<boolean>(false);
   const [user, setUser] = useState<IValue>({
+    valueData: "",
+    valueInput: "",
+  });
+  const [doc, setDoc] = useState<IValue>({
+    valueData: "",
+    valueInput: "",
+  });
+  // End
+
+  // Value
+  const [valueList, setValueList] = useState<IListInput[]>([]);
+  const [valuePage, setValuePage] = useState<Number>(1);
+  const [valueLoading, setValueLoading] = useState<boolean>(true);
+  const [valueMoreLoading, setValueMoreLoading] = useState<boolean>(false);
+  const [valueHasMore, setValueHasMore] = useState<boolean>(false);
+  const [value, setValue] = useState<IValue>({
+    valueData: "",
+    valueInput: "",
+  });
+  // End
+
+  const [createdBy, setCreatedBy] = useState<IValue>({
     valueData: LocalStorage.getUser()._id,
     valueInput: LocalStorage.getUser().name,
+  });
+  const [allow, setAllow] = useState<IValue>({
+    valueData: "",
+    valueInput: "",
   });
   const [name, setName] = useState<IValue>({
     valueData: "",
@@ -127,6 +181,120 @@ const FormPermissionPage: React.FC = () => {
 
       navigate("/permission");
     }
+  };
+
+  const getUser = async (search?: string): Promise<void> => {
+    try {
+      if (!userLoading) {
+        setUserMoreLoading(true);
+      } else {
+        setUserMoreLoading(false);
+      }
+
+      const result: any = await GetDataServer(DataAPI.USERS).FIND({
+        search: search ?? "",
+        limit: 10,
+        page: `${userPage}`,
+        filters: [["status", "=", "1"]],
+      });
+      if (result.data.length > 0) {
+        let listInput: IListInput[] = result.data.map((item: any) => {
+          return {
+            name: item.name,
+            value: item._id,
+            data: item,
+          };
+        });
+        setUserList([...userList, ...listInput]);
+        setUserHasMore(result.hasMore);
+
+        setUserPage(result.nextPage);
+      }
+
+      setUserLoading(false);
+      setUserMoreLoading(false);
+    } catch (error: any) {
+      setUserLoading(false);
+      setUserMoreLoading(false);
+      setUserHasMore(false);
+    }
+  };
+
+  const ResetUser = () => {
+    setUserList([]);
+    setUserHasMore(false);
+    setUserPage(1);
+    setUserLoading(true);
+  };
+
+  const getValue = async (data: {
+    search?: string | String;
+    refresh?: boolean;
+  }): Promise<void> => {
+    if (allow.valueData) {
+      try {
+        if (data.refresh === undefined) {
+          data.refresh = true;
+        }
+
+        let endpoint: DataAPI;
+
+        switch (allow.valueData) {
+          case "branch":
+            endpoint = DataAPI.BRANCH;
+            break;
+          case "user":
+            endpoint = DataAPI.USERS;
+            break;
+          case "customer":
+            endpoint = DataAPI.CUSTOMER;
+            break;
+          case "customergroup":
+            endpoint = DataAPI.GROUP;
+            break;
+          default:
+            endpoint = DataAPI.USERGROUP;
+            break;
+        }
+
+        const result: any = await GetDataServer(endpoint).FIND({
+          search: data.search ?? "",
+          limit: 10,
+          page: `${data.refresh ? 1 : valuePage}`,
+          filters: [["status", "=", "1"]],
+        });
+        if (result.data.length > 0) {
+          let listInput: IListInput[] = result.data.map((item: any) => {
+            return {
+              name: item.name,
+              value: item._id,
+            };
+          });
+
+          if (!data.refresh) {
+            setValueList([...valueList, ...listInput]);
+          } else {
+            setValueList([...listInput]);
+          }
+          setValueHasMore(result.hasMore);
+          setValuePage(result.nextPage);
+        }
+
+        setValueLoading(false);
+        setValueMoreLoading(false);
+      } catch (error: any) {
+        setValueLoading(false);
+        setValueMoreLoading(false);
+        setValueHasMore(false);
+      }
+    }
+  };
+
+  const ResetValue = () => {
+    setValueList([]);
+    setValueHasMore(false);
+    setValuePage(1);
+    setValueLoading(true);
   };
 
   const onDelete = (): void => {
@@ -295,48 +463,135 @@ const FormPermissionPage: React.FC = () => {
               </div>
             </div>
             <div className=" px-5 flex flex-col ">
-              <div className="border w-full flex-1  bg-white rounded-md overflow-y-scroll scrollbar-none">
+              <div className="border w-full flex-1  bg-white rounded-md ">
                 <div className="w-full h-auto  float-left rounded-md p-3 py-5">
                   <div className=" w-1/2 px-4 float-left ">
                     <InputComponent
                       mandatoy
-                      label="Name"
-                      value={name}
+                      infiniteScroll={{
+                        loading: userMoreLoading,
+                        hasMore: userHasMore,
+                        next: () => {
+                          getUser();
+                        },
+                        onSearch(e) {
+                          getUser(e);
+                        },
+                      }}
+                      loading={userLoading}
+                      onChange={(e) => {
+                        ResetUser();
+                        setUser({
+                          ...user,
+                          valueInput: e,
+                        });
+                      }}
+                      onSelected={(e: any) => {
+                        setUser({ valueData: e.value, valueInput: e.name });
+
+                        ResetUser();
+                      }}
+                      onReset={() => {
+                        ResetUser();
+                        setUser({
+                          valueData: null,
+                          valueInput: "",
+                        });
+                      }}
+                      modalStyle="mt-2"
+                      list={userList}
+                      label="User"
+                      value={user}
                       className="h-[38px] mb-3"
                       type="text"
-                      onChange={(e) =>
-                        setName({
-                          valueData: e,
-                          valueInput: e,
-                        })
-                      }
                       disabled={
                         id != null ? (status !== "Draft" ? true : false) : false
                       }
                     />
-
                     <InputComponent
-                      label="Created By"
-                      value={user}
-                      className="h-[38px]  mb-3"
+                      mandatoy
+                      label="Allow"
+                      value={allow}
+                      className="h-[38px] mb-3"
+                      modalStyle="mt-2"
+                      type="text"
                       onChange={(e) =>
-                        setUser({
+                        setAllow({
                           valueData: e,
                           valueInput: e,
                         })
                       }
-                      disabled
-                    />
-                    <label className="text-sm">Desc</label>
-                    <textarea
-                      className="border mt-1 p-2 text-[0.95em] bg-gray-50  w-full rounded-md h-[150px]"
-                      name="Site Uri"
-                      value={desc}
-                      onChange={(e) => setDesc(e.target.value)}
+                      onReset={() => {
+                        setAllow({ valueData: "", valueInput: "" });
+                        setValue({ valueData: "", valueInput: "" });
+                        ResetValue();
+                      }}
                       disabled={
                         id != null ? (status !== "Draft" ? true : false) : false
                       }
+                      onSelected={(e) => {
+                        setAllow({ valueData: e.value, valueInput: e.name });
+                        setValue({ valueData: "", valueInput: "" });
+                        ResetValue();
+                      }}
+                      list={allowList}
                     />
+                    {allow.valueData && (
+                      <InputComponent
+                        mandatoy
+                        infiniteScroll={{
+                          loading: valueMoreLoading,
+                          hasMore: valueHasMore,
+                          next: () => {
+                            setValueMoreLoading(true);
+                            getValue({
+                              refresh: false,
+                              search: value.valueInput,
+                            });
+                          },
+                          onSearch(e) {
+                            ResetValue();
+                            getValue({ refresh: true, search: e });
+                          },
+                        }}
+                        onCLick={() => {
+                          ResetValue();
+                          getValue({
+                            refresh: true,
+                            search: value.valueInput,
+                          });
+                        }}
+                        loading={valueLoading}
+                        onChange={(e) => {
+                          setValue({
+                            ...value,
+                            valueInput: e,
+                          });
+                        }}
+                        onSelected={(e) => {
+                          setValue({ valueData: e.value, valueInput: e.name });
+                        }}
+                        onReset={() => {
+                          setValue({
+                            valueData: "",
+                            valueInput: "",
+                          });
+                        }}
+                        list={valueList}
+                        label="For Value"
+                        value={value}
+                        className="h-[38px] mb-3"
+                        type="text"
+                        modalStyle="mt-2"
+                        disabled={
+                          id != null
+                            ? status !== "Draft"
+                              ? true
+                              : false
+                            : false
+                        }
+                      />
+                    )}
                   </div>
                   <div className=" w-1/2 px-4 float-left  mb-3">
                     <InputComponent
@@ -353,21 +608,69 @@ const FormPermissionPage: React.FC = () => {
                       disabled
                     />
                     <InputComponent
-                      label="Status"
-                      value={{ valueData: status, valueInput: status }}
+                      label="Created By"
+                      value={createdBy}
                       className="h-[38px]  mb-3"
+                      disabled
+                    />
+                    {id && (
+                      <InputComponent
+                        label="Status"
+                        value={{ valueData: status, valueInput: status }}
+                        className="h-[38px]  mb-3"
+                        type="text"
+                        onChange={(e) =>
+                          setCreatedAt({
+                            valueData: e,
+                            valueInput: e,
+                          })
+                        }
+                        disabled
+                      />
+                    )}
+                  </div>
+                </div>
+              </div>
+              <ToggleBodyComponent
+                name="Advance Control"
+                className="mt-5"
+                child={
+                  <div className="w-1/2">
+                    <div className="flex item-center text-sm mb-4 text-gray-800">
+                      <input
+                        // checked={allDoc}
+                        // onChange={() => setAlldoc(!allDoc)}
+                        type="checkbox"
+                        className=" mt-[2px] mr-2"
+                      />
+                      <h4>Apply To All Document Types</h4>
+                    </div>
+                    <InputComponent
+                      mandatoy
+                      label="Applicable For"
+                      value={doc}
+                      className="h-[38px] mb-3"
                       type="text"
                       onChange={(e) =>
-                        setCreatedAt({
+                        setDoc({
                           valueData: e,
                           valueInput: e,
                         })
                       }
-                      disabled
+                      onReset={() => {
+                        setDoc({ valueData: "", valueInput: "" });
+                      }}
+                      disabled={
+                        id != null ? (status !== "Draft" ? true : false) : false
+                      }
+                      onSelected={(e) => {
+                        setDoc({ valueData: e.value, valueInput: e.name });
+                      }}
+                      list={docList}
                     />
                   </div>
-                </div>
-              </div>
+                }
+              />
 
               <TimeLineVertical data={history} />
             </div>
