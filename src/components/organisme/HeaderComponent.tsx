e521a313d9b8ebf71b3c9087c4cedc59178198a8
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-09-26 15:13:57 +0700

    update

--- src/components/organisme/HeaderComponent.tsx
+++ src/components/organisme/HeaderComponent.tsx
@@ -46,7 +46,7 @@ const HeaderComponent: React.FC = () => {
       <div className="text-sm flex items-center ml-3">
         <SeacrhHeaderComponent />
 
-        <IconMenuHeader Icon={NotificationsNoneIcon} title="notif" />
+        {/* <IconMenuHeader Icon={NotificationsNoneIcon} title="notif" /> */}
       </div>
 
       <div className="flex items-center">
