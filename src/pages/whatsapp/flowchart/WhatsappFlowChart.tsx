3f239726f36842d6411e9a2959bf45f937357071
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-09-29 17:08:28 +0700

    update

--- src/pages/whatsapp/flowchart/WhatsappFlowChart.tsx
+++ src/pages/whatsapp/flowchart/WhatsappFlowChart.tsx
@@ -1,20 +1,37 @@
 import React, { useCallback } from "react";
-import ReactFlow, { useNodesState, useEdgesState, addEdge,Background,Panel } from "reactflow";
+import ReactFlow, {
+  useNodesState,
+  useEdgesState,
+  addEdge,
+  Background,
+  Controls,
+} from "reactflow";
 
 import "reactflow/dist/style.css";
 const WhatsappFlowChart = () => {
   const initialNodes = [
-    { id: "1", position: { x: 100, y: 20 }, data: { label: "Start" } },
-    { id: "2", position: { x: 20, y: 100 }, data: { label: "Insert" } },
+    { id: "welcome", position: { x: 120, y: 20 }, data: { label: "Start" } },
+    { id: "Product", position: { x: 20, y: 100 }, data: { label: "Product" } },
+    { id: "Branch", position: { x: 220, y: 100 }, data: { label: "Branch" } },
+    { id: "Bogor", position: { x: 220, y: 200 }, data: { label: "Bogor" } },
+    { id: "Depok", position: { x: 400, y: 200 }, data: { label: "Depok" } },
   ];
-  const initialEdges = [{ id: "e1-2", source: "1", target: "2" }];
+  const initialEdges = [
+    { id: "e1-2", source: "welcome", target: "Product" },
+    { id: "e1-2", source: "welcome", target: "Branch" },
+    { id: "e1-2", source: "Branch", target: "Bogor" },
+    { id: "e1-2", source: "Branch", target: "Depok" },
+  ];
 
   const [nodes, setNodes, onNodesChange] = useNodesState(initialNodes);
   const [edges, setEdges, onEdgesChange] = useEdgesState(initialEdges);
 
-  const onConnect = useCallback((params:any) => setEdges((eds) => addEdge(params, eds)), [setEdges]);
+  const onConnect = useCallback(
+    (params: any) => setEdges((eds) => addEdge(params, eds)),
+    [setEdges]
+  );
   return (
-    <div style={{ width: "100%", height: "100vh" }}>
+    <div style={{ width: "100%", height: "90vh" }}>
       <ReactFlow
         nodes={nodes}
         edges={edges}
@@ -22,8 +39,8 @@ const WhatsappFlowChart = () => {
         onEdgesChange={onEdgesChange}
         onConnect={onConnect}
       >
-          <Background />
-          
+        <Background />
+        {/* <Controls /> */}
       </ReactFlow>
     </div>
   );
