f4bdb493ebefaa375770a2e19d8f4fe51e66af3e
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-10-02 09:07:31 +0700

    update

--- src/pages/whatsapp/flowchart/chartcomponents/ChartResponseInput.tsx
+++ src/pages/whatsapp/flowchart/chartcomponents/ChartResponseInput.tsx
@@ -1,11 +1,12 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { Handle, Position } from "reactflow";
 import RepeatOutlinedIcon from "@mui/icons-material/RepeatOutlined";
 const handleStyle = { left: 10 };
 
 const ChartResponseInput = (isConnectable: any) => {
+  const [value, setValue] = useState<string>("");
   const onChange = useCallback((evt: any) => {
-    console.log(evt.target.value);
+    setValue(evt.target.value);
   }, []);
 
   return (
@@ -18,14 +19,19 @@ const ChartResponseInput = (isConnectable: any) => {
       <div className="w-full flex flex-col">
         <div className="flex justify-between items-center">
           <h4 className="text-[0.8em] mb-1">Bot Response Type</h4>
-          <RepeatOutlinedIcon style={{marginTop:"-5px", fontSize: 17}} className="opacity-60 hover:opacity-90 duration-300" />
+          <RepeatOutlinedIcon
+            style={{ marginTop: "-5px", fontSize: 17 }}
+            className="opacity-60 hover:opacity-90 duration-300"
+          />
         </div>
         <textarea
           name=""
           id=""
           rows={4}
+          onChange={(e) => ChartResponseInput(e)}
           className=" rounded-md border font-normal text-[0.8em] p-2 border-gray-500"
-          value={`Halo ada yang bisa kami bantu?`}
+          value={value}
+          placeholder=""
         ></textarea>
       </div>
       <Handle
--- src/pages/whatsapp/flowchart/chartcomponents/ChartWelcomeInput.tsx
+++ src/pages/whatsapp/flowchart/chartcomponents/ChartWelcomeInput.tsx
@@ -1,11 +1,14 @@
-import { useCallback } from "react";
+import { useCallback, useState } from "react";
 import { Handle, Position } from "reactflow";
 
 const handleStyle = { left: 10 };
 
 const ChartWelcomeInput = (isConnectable: any) => {
+  const [value, setValue] = useState<string>("");
+
   const onChange = useCallback((evt: any) => {
-    console.log(evt.target.value);
+    // console.log(evt.target.value);
+    setValue(evt.target.value);
   }, []);
 
   return (
@@ -18,11 +21,13 @@ const ChartWelcomeInput = (isConnectable: any) => {
       <div className="w-full flex flex-col">
         <h4 className="text-[0.8em] mb-1">Welcome Message</h4>
         <textarea
+          onChange={(e) => onChange(e)}
           name=""
           id=""
           rows={4}
           className=" rounded-md border font-normal text-[0.8em] p-2 border-gray-500"
-          value={`Halo ada yang bisa kami bantu?`}
+          value={value}
+          placeholder="Halo ada yang bisa kami bantu?"
         ></textarea>
       </div>
       <Handle
