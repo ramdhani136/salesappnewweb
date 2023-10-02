8c2e0b33f43a282e5c67f431cd3abd06d992d114
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2023-10-02 09:08:38 +0700

    update

--- src/pages/whatsapp/flowchart/chartcomponents/ChartResponseInput.tsx
+++ src/pages/whatsapp/flowchart/chartcomponents/ChartResponseInput.tsx
@@ -28,7 +28,7 @@ const ChartResponseInput = (isConnectable: any) => {
           name=""
           id=""
           rows={4}
-          onChange={(e) => ChartResponseInput(e)}
+          onChange={(e) => onChange(e)}
           className=" rounded-md border font-normal text-[0.8em] p-2 border-gray-500"
           value={value}
           placeholder=""
