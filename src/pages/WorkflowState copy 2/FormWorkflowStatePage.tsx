c5f30c01e89939105df6ee931c3c93c4f7718c2b
Ilham Ramdhani <44194409+ramdhani136@users.noreply.github.com>
2024-02-06 11:18:43 +0700

    update

---
+++ src/pages/WorkflowState copy 2/FormWorkflowStatePage.tsx
@@ -0,0 +1,347 @@
+import { useNavigate, useParams } from "react-router-dom";
+import { useEffect, useState } from "react";
+import GetDataServer, { DataAPI } from "../../utils/GetDataServer";
+import MoreHorizIcon from "@mui/icons-material/MoreHoriz";
+import {
+  ButtonStatusComponent,
+  IconButton,
+  InputComponent,
+  Select,
+} from "../../components/atoms";
+import { IValue } from "../../components/atoms/InputComponent";
+import { LoadingComponent } from "../../components/moleculs";
+import moment from "moment";
+import { AlertModal, LocalStorage, Meta } from "../../utils";
+
+import { IListIconButton } from "../../components/atoms/IconButton";
+import Swal from "sweetalert2";
+import { modalSet } from "../../redux/slices/ModalSlice";
+import { useDispatch } from "react-redux";
+
+const FromWorkflowState: React.FC<any> = ({ props }) => {
+  const modal = props ? props.modal ?? false : false;
+  let { id } = useParams();
+  const [data, setData] = useState<any>({});
+  const metaData = {
+    title: `${id ? data.name : "New Workflow"} - Sales App Ekatunggal`,
+    description: "Halaman form Workflow - Sales web system",
+  };
+
+  const navigate = useNavigate();
+
+  const [scroll, setScroll] = useState<number>(0);
+
+  const [isChangeData, setChangeData] = useState<boolean>(false);
+
+  const [user, setUser] = useState<IValue>({
+    valueData: LocalStorage.getUser()._id,
+    valueInput: LocalStorage.getUser().name,
+  });
+  const [name, setName] = useState<IValue>({
+    valueData: "",
+    valueInput: "",
+  });
+
+  const [status, setStatus] = useState<string>("1");
+
+  const statusType: any[] = [
+    { title: "Disabled", value: "0" },
+    { title: "Enabled", value: "1" },
+  ];
+
+  const [prevData, setPrevData] = useState<any>({
+    name: name.valueData,
+    status: status,
+  });
+
+  const [createdAt, setCreatedAt] = useState<IValue>({
+    valueData: moment(Number(new Date())).format("YYYY-MM-DD"),
+    valueInput: moment(Number(new Date())).format("YYYY-MM-DD"),
+  });
+
+  const dispatch = useDispatch();
+  const [loading, setLoading] = useState<boolean>(true);
+
+  const [listMoreAction, setListMoreAction] = useState<IListIconButton[]>([]);
+
+  const getData = async (): Promise<void> => {
+    try {
+      const result = await GetDataServer(DataAPI.WORKFLOWSTATE).FINDONE(
+        `${id}`
+      );
+
+      setStatus(result.data.status);
+
+      setName({
+        valueData: result.data.name,
+        valueInput: result.data.name,
+      });
+      setUser({
+        valueData: result.data.user._id,
+        valueInput: result.data.user.name,
+      });
+      setCreatedAt({
+        valueData: moment(result.data.createdAt).format("YYYY-MM-DD"),
+        valueInput: moment(result.data.createdAt).format("YYYY-MM-DD"),
+      });
+
+      setData(result.data);
+
+      setPrevData({
+        name: result.data.name,
+        status: result.data.status,
+      });
+
+      setLoading(false);
+    } catch (error: any) {
+      setLoading(false);
+      AlertModal.Default({
+        icon: "error",
+        title: "Error",
+        text: "Data not found!",
+      });
+
+      navigate("/workflowstate");
+    }
+  };
+
+  const onDelete = (): void => {
+    if (id) {
+      const progress = async (): Promise<void> => {
+        setLoading(true);
+        try {
+          await GetDataServer(DataAPI.WORKFLOWSTATE).DELETE(`${id}`);
+          navigate("/workflowstate");
+        } catch (error: any) {
+          setLoading(false);
+          Swal.fire(
+            "Error!",
+            `${
+              error.response.data.msg
+                ? error.response.data.msg
+                : error.message
+                ? error.message
+                : "Error Delete"
+            }`,
+            "error"
+          );
+        }
+      };
+
+      AlertModal.confirmation({ onConfirm: progress });
+    }
+  };
+
+  const onSave = async (nextState?: String): Promise<any> => {
+    setLoading(true);
+    try {
+      let data: any = {
+        name: name.valueData,
+        status: status,
+      };
+      if (nextState) {
+        data.nextState = nextState;
+      }
+
+      let Action =
+        id && !modal
+          ? GetDataServer(DataAPI.WORKFLOWSTATE).UPDATE({ id: id, data: data })
+          : GetDataServer(DataAPI.WORKFLOWSTATE).CREATE(data);
+
+      const result = await Action;
+
+      if (id && !modal) {
+        getData();
+        Swal.fire({ icon: "success", text: "Saved" });
+      } else if (modal) {
+        props.Callback(result.data?.data ?? {});
+        dispatch(
+          modalSet({
+            active: false,
+            Children: null,
+            title: "",
+            props: {},
+            className: "",
+          })
+        );
+      } else {
+        navigate(`/workflowstate/${result.data.data._id}`);
+        navigate(0);
+      }
+    } catch (error: any) {
+      Swal.fire(
+        "Error!",
+        `${
+          error.response.data.msg
+            ? error.response.data.msg
+            : error.message
+            ? error.message
+            : "Error Insert"
+        }`,
+        "error"
+      );
+    }
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    if (id && !modal) {
+      getData();
+      setListMoreAction([{ name: "Delete", onClick: onDelete }]);
+    } else {
+      setLoading(false);
+      setListMoreAction([]);
+    }
+
+    if (modal) {
+      setName({ valueData: props.name, valueInput: props.name });
+    }
+  }, []);
+
+  // Cek perubahan
+  useEffect(() => {
+    const actualData = {
+      name: name.valueData,
+      status: status,
+    };
+    if (JSON.stringify(actualData) !== JSON.stringify(prevData)) {
+      setChangeData(true);
+    } else {
+      setChangeData(false);
+    }
+  }, [name, status]);
+  // End
+
+  return (
+    <>
+      {Meta(metaData)}
+      <div
+        className="  max-h-[calc(100vh-70px)] overflow-y-auto scrollbar-thin scrollbar-track-gray-200 scrollbar-thumb-gray-300"
+        onScroll={(e: any) => setScroll(e.target.scrollTop)}
+      >
+        {!loading ? (
+          <>
+            <div
+              className={`w-full flex  justify-between px-5 ${
+                scroll > 0
+                  ? "bg-white border-b border-gray-100 drop-shadow-sm py-6"
+                  : "bg-gray-100"
+              } py-5 sticky top-0 z-[51] duration-500`}
+            >
+              <div className="flex  items-center">
+                <h4
+                  onClick={() => navigate("/workflowstate")}
+                  className="font-bold text-lg mr-2 cursor-pointer"
+                >
+                  {!id ? "New State" : data.name}
+                </h4>
+                <div className="text-md">
+                  {modal ? (
+                    "Form Workflow State"
+                  ) : (
+                    <ButtonStatusComponent
+                      // className="text-[0.7em]"
+                      status={data.status ?? "0"}
+                      name={data.status === "0" ? "Disabled" : "Enabled"}
+                    />
+                  )}
+                </div>
+              </div>
+              <div className="flex">
+                {listMoreAction.length > 0 && (
+                  <IconButton
+                    classModal="top-[29px]"
+                    primary
+                    Icon={MoreHorizIcon}
+                    iconSize={15}
+                    classIcon="mt-1"
+                    list={listMoreAction}
+                    iconListDisabled
+                    className={` duration-100 mr-2 px-2 `}
+                  />
+                )}
+
+                {isChangeData && (
+                  <IconButton
+                    name={id && !modal ? "Update" : "Save"}
+                    callback={() => {
+                      AlertModal.confirmation({
+                        onConfirm: onSave,
+                        confirmButtonText: `Yes, ${
+                          !id ? "Save it!" : "Update It"
+                        }`,
+                      });
+                    }}
+                    className={`opacity-80 hover:opacity-100 duration-100  `}
+                  />
+                )}
+              </div>
+            </div>
+            <div className=" px-5 flex flex-col ">
+              <div className="border w-full flex-1  bg-white rounded-md overflow-y-scroll scrollbar-none">
+                <div className="w-full h-auto  float-left rounded-md p-3 py-5">
+                  <div className=" w-1/2 px-4 float-left ">
+                    <InputComponent
+                      mandatoy
+                      label="Name"
+                      value={name}
+                      className="h-[38px] mb-3"
+                      type="text"
+                      onChange={(e) =>
+                        setName({
+                          valueData: e,
+                          valueInput: e,
+                        })
+                      }
+                      // disabled={
+                      //   id != null ? (status !== "Draft" ? true : false) : false
+                      // }
+                    />
+
+                    <InputComponent
+                      label="Created By"
+                      value={user}
+                      className="h-[38px]  mb-3"
+                      onChange={(e) =>
+                        setUser({
+                          valueData: e,
+                          valueInput: e,
+                        })
+                      }
+                      disabled
+                    />
+                  </div>
+                  <div className=" w-1/2 px-4 float-left  mb-3">
+                    <InputComponent
+                      label="Date"
+                      value={createdAt}
+                      className="h-[38px]  mb-3"
+                      type="date"
+                      onChange={(e) =>
+                        setCreatedAt({
+                          valueData: e,
+                          valueInput: e,
+                        })
+                      }
+                      disabled
+                    />
+                    <Select
+                      title="Status"
+                      data={statusType}
+                      value={status}
+                      setValue={setStatus}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </>
+        ) : (
+          <LoadingComponent />
+        )}
+      </div>
+    </>
+  );
+};
+
+export default FromWorkflowState;
