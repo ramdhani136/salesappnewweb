4acfeecb22c00d80885b7087ef54e200b949b848
ramdhani136 <ramdhaniit@gmail.com>
2024-03-06 16:45:50 +0700

    udate

--- src/pages/Schedule/ListItemSchedule.tsx
+++ src/pages/Schedule/ListItemSchedule.tsx
@@ -438,6 +438,7 @@ const ListItemSchedule: React.FC<IProps> = ({ props }) => {
           customer: item.customer.name,
           group: item.customerGroup.name,
           branch: item.branch.name,
+          notes: item?.notes ?? "",
           status: item.status == "0" ? "Open" : "Closed",
           createdBy: item.createdBy.name,
         };
