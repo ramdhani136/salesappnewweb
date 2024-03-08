f09821b661d17f3ffbecde5023d1ff033126e520
ramdhani136 <ramdhaniit@gmail.com>
2024-03-08 12:28:25 +0700

    update

--- src/pages/visit/VisitPage.tsx
+++ src/pages/visit/VisitPage.tsx
@@ -263,11 +263,11 @@ export const VisitPage: React.FC = (): any => {
         };
       });
 
-      const ws = XLSX.utils.json_to_sheet(getDataExport);
-      const wb = XLSX.utils.book_new();
-      XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
+      // const ws = XLSX.utils.json_to_sheet(getDataExport);
+      // const wb = XLSX.utils.book_new();
+      // XLSX.utils.book_append_sheet(wb, ws, "Sheet1");
 
-      XLSX.writeFile(wb, `visit.xlsx`);
+      // XLSX.writeFile(wb, `visit.xlsx`);
     } catch (error) {
       console.log(error);
     }
